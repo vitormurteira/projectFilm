08a730f638e59a141b3c89383a831e6601b6fedf
Vitor Murteira <vitormurteira@netcabo.pt>
2017-07-20 10:58:13 +0200

    file updates

---
+++ inc/config.list.php
@@ -0,0 +1,13 @@
+<?php
+
+// Tableau contenant des données sur la configuration
+$config = array(
+	'db_host' => 'wf3.progweb.fr',
+	'db_user' => 'julienb',
+	'db_password' => 'webforce3',
+	'db_database' => 'movies_database'
+);
+
+// J'inclus composer, db.php & functions.php
+require dirname(dirname(__FILE__)).'/vendor/autoload.php';
+require dirname(__FILE__).'/functions.php';
