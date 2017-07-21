ee11fe70920053337e95398e628b4be94460bc8b
theseeconsulting <jb@theseeconsulting.com>
2017-07-21 11:54:25 +0200

    footer customization

---
+++ view/footer.php
@@ -0,0 +1,18 @@
+    <div class="panel panel-default">
+      <div class="panel-body text-center">
+       <!-- &copy; <?= date('Y') ?> | Tous droits réservés<br>-->
+        <a href="<?= $socialLinksPage->facebook->shareUrl ?>">Facebook</a> |
+        <a href="<?= $socialLinksPage->twitter->shareUrl ?>">Twitter</a> |
+        <a href="<?= $socialLinksPage->linkedin->shareUrl ?>">LinkedIn</a>
+      </div>
+    </div>
+  </div>  
+<br>
+<br>
+© 2017 | Vitor, international man of mystery && Thesee Consulting Corporation - all right reserved
+	
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  	 
+  </body>
+</html>
