8bd29426811801d84d65b5994fac72cd283a698a
theseeconsulting <jb@theseeconsulting.com>
2017-07-21 10:29:39 +0200

    definition de la $currentPage

--- inc/config.list.php
+++ inc/config.list.php
@@ -8,6 +8,12 @@ $config = array(
 	'db_database' => 'movies_database'
 );
 
+// definition de la variable $currentPage utilisée dans la partie "header"
+if (empty($currentPage)) {
+	$currentPage = 'home';
+}
+
+
 // J'inclus composer, db.php & functions.php
 require dirname(dirname(__FILE__)).'/vendor/autoload.php';
 require dirname(__FILE__).'/functions.php';
