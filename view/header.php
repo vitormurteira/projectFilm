673b7ff49f998c8c0d2fdfc439cc10b3dc321292
theseeconsulting <jb@theseeconsulting.com>
2017-07-21 11:38:14 +0200

    header customization

---
+++ view/header.php
@@ -0,0 +1,66 @@
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <h1>Movies Project</h1>
+      <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+      <!-- Optional theme -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+      <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+
+      <style type="text/css">
+        body { padding-top: 70px; }
+      </style>
+  </head>
+
+  <body>
+
+    <br>
+    <br>
+    <br>
+
+    <div class="container">
+
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <a class="navbar-brand" href="./">Home</a>
+              </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+<!--la $currentPage est defini dans le dossier inc, fichier "config" de la maniere suivante:
+if (empty($currentPage)) {
+  $currentPage = 'home';
+}.... -->
+
+          <ul class="nav nav-pills">
+              <li<?= $currentPage == 'home' ? ' class="active"' : '' ?>><a href="index.php">Accueil</a></li>
+              <li<?= $currentPage == 'list' ? ' class="active"' : '' ?>><a href="list.php">Catégories</a></li>
+              <li<?= $currentPage == 'film' ? ' class="active"' : '' ?>><a href="film.php">Ajouter un film</a></li>
+        	</ul>
+
+    <!--ajout d'un questionnaire de recherche-->
+
+        <form action="list.php" class="navbar-form navbar-right">
+            <div class="form-group">
+              <input type="text" name="s" class="form-control" placeholder="Recherche">
+            </div>
+          <button type="submit" class="btn btn-default">OK</button>
+        </form>
+
+    </div>
+
+  <br>
+  <br>
+  <br>
+  <br>
