661ddb2a098c0fe7ac8ca18a64be1f4a65a56ce4
theseeconsulting <jb@theseeconsulting.com>
2017-07-21 12:12:08 +0200

    index customization

---
+++ public/index.php
@@ -0,0 +1,34 @@
+
+
+<?php
+
+// J'inclus le fichier de config
+require '../inc/config.php';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// J'inclus les vues
+
+require dirname(dirname(__FILE__)).'/view/header.php';
+require dirname(dirname(__FILE__)).'/view/home.php';
+require dirname(dirname(__FILE__)).'/view/footer.php';
